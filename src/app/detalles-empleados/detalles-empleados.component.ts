0116dccd757bffe239a4db8a0aa536c087c0a889
Saed Martínez <shmartinez4@misena.edu.co>
2021-11-26 17:24:00 -0500

    first floor

--- src/app/detalles-empleados/detalles-empleados.component.ts
+++ src/app/detalles-empleados/detalles-empleados.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Empleado } from '../empleado';
+import { EmpleadosService } from '../empleados.service';
 
 @Component({
   selector: 'app-detalles-empleados',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetallesEmpleadosComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  empleado: Empleado;
+
+  constructor(private route: ActivatedRoute, private eService: EmpleadosService, private router: Router) { }
 
   ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.obtenerInfoEmploy(this.id);
   }
+
+  obtenerInfoEmploy(inID: number) {
+    this.eService.buscarEmpleadoID(inID).subscribe(dato => {
+      this.empleado = dato;
+    });
+  }
+
+
 
 }
